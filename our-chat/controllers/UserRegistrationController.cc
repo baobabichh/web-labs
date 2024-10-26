1f596827861d488bb732864f398034ae59ea78b5
baobabichh <pickup2legs@gmail.com>
2024-10-26 22:59:45 +0200

    changes

--- our-chat/controllers/UserRegistrationController.cc
+++ our-chat/controllers/UserRegistrationController.cc
@@ -79,3 +79,68 @@ void UserRegistrationController::registerUser(const HttpRequestPtr &req, std::fu
         return;
     }
 }
+
+void UserRegistrationController::loginUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+{
+    std::string email = req->getParameter("email");
+    std::string password = req->getParameter("password");
+
+    if(email.size() < 5)
+    {
+        Json::Value jsonResponse;
+        jsonResponse["Message"] = "email.size() < 5";
+        jsonResponse["Status"] = "Fail";
+        auto response = HttpResponse::newHttpJsonResponse(jsonResponse);
+        response->setStatusCode(k200OK);
+        callback(response);
+        return;
+    }
+
+    if(password.size() < 5)
+    {
+        Json::Value jsonResponse;
+        jsonResponse["Message"] = "password.size() < 5";
+        jsonResponse["Status"] = "Fail";
+        auto response = HttpResponse::newHttpJsonResponse(jsonResponse);
+        response->setStatusCode(k200OK);
+        callback(response);
+        return;
+    }
+
+    auto dbClient = app().getDbClient("our_chat");
+
+    try 
+    {
+        orm::Result result = dbClient->execSqlSync("SELECT * FROM Users where Email = ? and Password", email, password, 1);
+
+        for (auto row : result)
+        {
+            Json::Value jsonResponse;
+            jsonResponse["Status"] = "Success";
+            auto response = HttpResponse::newHttpJsonResponse(jsonResponse);
+            response->setStatusCode(k200OK);
+            callback(response);
+            return;
+        }
+    }
+    catch (const orm::DrogonDbException &e)
+    {
+        Json::Value jsonResponse;
+        jsonResponse["Message"] = e.base().what();
+        jsonResponse["Status"] = "Fail";
+        auto response = HttpResponse::newHttpJsonResponse(jsonResponse);
+        response->setStatusCode(k200OK);
+        callback(response);
+        return;
+    }
+
+    {
+        Json::Value jsonResponse;
+        jsonResponse["Message"] = e.base().what();
+        jsonResponse["Status"] = "Fail";
+        auto response = HttpResponse::newHttpJsonResponse(jsonResponse);
+        response->setStatusCode(k200OK);
+        callback(response);
+        return;
+    }
+}
