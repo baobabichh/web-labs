7133fd387561aab9192c1789b69f9813962874b4
goshan9 <pickup2legs@gmail.com>
2024-12-11 21:40:42 +0100

    changes

--- prebuild.sh
+++ prebuild.sh
@@ -27,45 +27,6 @@ sudo apt-get install -y build-essential autoconf libtool pkg-config
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-git clone https://github.com/abseil/abseil-cpp.git
-cd abseil-cpp
-
-mkdir build
-cd build
-cmake ..
-make
-sudo make install
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
-cd ..
-cd ..
-
-export MY_INSTALL_DIR=$HOME/.local
-mkdir -p $MY_INSTALL_DIR
-export PATH="$MY_INSTALL_DIR/bin:$PATH"
-
-rm -rf grpc/* grpc/.*
-rmdir grpc
-git clone --recurse-submodules -b v1.66.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
-cd grpc
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_INSTALL=ON \
-      -DgRPC_BUILD_TESTS=OFF \
-      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      ../..
-make -j 4
-make install
-popd
-
-cd ..
-echo "Now in $(pwd)"
-
-sudo apt-get update -y
-sudo apt-get upgrade -y
-
 sudo apt-get install -y git gcc g++ cmake
 sudo apt-get update -y
 sudo apt-get upgrade -y
@@ -141,30 +102,3 @@ sudo apt-get upgrade -y
 
 cd ..
 cd ..
-
-
-
-git clone --recurse-submodules -b v1.66.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
-
-cd grpc
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_INSTALL=ON \
-      -DgRPC_BUILD_TESTS=OFF \
-      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      ../..
-make -j 4
-make install
-popd
-
-
-cd examples/cpp/helloworld
-
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
-make -j 4
-
-
-cd ~/web/web-labs
-./build_protos.sh
