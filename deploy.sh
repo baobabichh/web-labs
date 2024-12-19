1eef94a8e6d584eb792fc4b6b39df84223ff0b0b
goshan9 <bossolga1999@gmail.com>
2024-12-20 00:35:20 +0100

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd our-chat
-sudo nohup ./rebuild.sh > chat_output.log 
+sudo nohup ./rebuild.sh > chat_output.log 2>&1 &
 
 cd ..
 
 cd image_service/build
-sudo nohup ./image_service > immg_output.log
+sudo nohup ./image_service > immg_output.log 2>&1 &
