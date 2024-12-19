bcd598551b406ce5c1c2c5ff588e744d22ffa721
goshan9 <bossolga1999@gmail.com>
2024-12-20 00:32:47 +0100

    changes

--- build_protos.sh
+++ build_protos.sh
@@ -1,2 +1,4 @@
+mkdir generated_protos
+
 ./grpc/cmake/build/third_party/protobuf/protoc --cpp_out=generated_protos protos/image_service.proto 
 ./grpc/cmake/build/third_party/protobuf/protoc --cpp_out=generated_protos --grpc_out=generated_protos --plugin=protoc-gen-grpc=$(which grpc_cpp_plugin) protos/image_service.proto
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd our-chat
-sudo nohup ./rebuild.sh > chat_output.log 2>&1 &
+sudo nohup ./rebuild.sh > chat_output.log 
 
 cd ..
 
 cd image_service/build
-sudo nohup ./image_service > immg_output.log 2>&1 &
+sudo nohup ./image_service > immg_output.log
